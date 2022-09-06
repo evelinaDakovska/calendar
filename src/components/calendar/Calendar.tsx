71b37d6ccefdba84a3880da5fa8f1beaa544ff62
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-09-06 12:24:57 +0300

    feat/change month and styles implemented

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import styles from "./Calendar.module.scss";
 import { DayCell } from "./DayCell";
@@ -6,8 +6,9 @@ import { DayName } from "./DayName";
 
 export const Calendar = () => {
   const [today, setToday] = useState<Date>();
-  const [month, setMonth] = useState<number>();
-  const [firstDay, setFirstDay] = useState<number>();
+  const [month, setMonth] = useState<number>(0);
+  const [year, setYear] = useState<number>(0);
+  const [firstDay, setFirstDay] = useState<number>(0);
   const [coefficient, setCoefficient] = useState<number>(0);
 
   useEffect(() => {
@@ -17,6 +18,7 @@ export const Calendar = () => {
   useEffect(() => {
     if (today) {
       setMonth(today.getMonth());
+      setYear(today.getFullYear());
     }
   }, [today]);
 
@@ -42,18 +44,44 @@ export const Calendar = () => {
       return <DayName key={uuidv4()} name={dayName} />;
     });
 
-  const days = Array(35)
-    .fill(true)
-    .map((current, index) => {
-      const currentDate = new Date();
-      currentDate.setDate(index + coefficient);
-      return <DayCell key={uuidv4()} day={currentDate} />;
-    });
+  const days = useMemo(
+    () =>
+      year !== 0 &&
+      month !== 0 &&
+      Array(35)
+        .fill(true)
+        .map((current, index) => {
+          const currentDate = new Date(year, month);
+          currentDate.setDate(index + coefficient);
+          return <DayCell key={uuidv4()} day={currentDate} month={month} />;
+        }),
+    [year, month, coefficient]
+  );
 
   return (
-    <div className={styles.calendar}>
-      {dayNames}
-      {days}
-    </div>
+    <>
+      <button
+        className={styles.changeMonth}
+        onClick={() => setMonth((prev) => prev - 1)}
+      >
+        previous month
+      </button>
+      <button
+        className={styles.changeMonth}
+        onClick={() => setMonth((prev) => prev + 1)}
+      >
+        next month
+      </button>
+      <h3>
+        {new Date(year, month).toLocaleString("en-US", {
+          month: "long",
+        })}{" "}
+        {year}
+      </h3>
+      <div className={styles.calendar}>
+        {dayNames}
+        {days}
+      </div>
+    </>
   );
 };
--- src/components/calendar/DayCell.tsx
+++ src/components/calendar/DayCell.tsx
@@ -1,19 +1,19 @@
 import { v4 as uuidv4 } from "uuid";
 import styles from "./Calendar.module.scss";
 
-export const DayCell = ({ day }: { day: Date }) => {
-  const month = new Date().getMonth();
-  const dayC = new Date().getDate();
-  const classNameMonth =
-    month === day.getMonth()
-      ? day.getDate() === dayC
+export const DayCell = ({ day, month }: { day: Date; month: number }) => {
+  const monthC = new Date(day).getMonth();
+  const today = new Date().toDateString();
+  const className =
+    monthC === month
+      ? day.toDateString() === today
         ? "today"
         : ""
       : "other-month";
   return (
     <div
       key={uuidv4()}
-      className={`${styles["calendar-cell"]} ${styles[classNameMonth]}`}
+      className={`${styles["calendar-cell"]} ${styles[className]}`}
     >
       {day.getDate()}
     </div>
